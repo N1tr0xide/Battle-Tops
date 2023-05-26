f2cd0f632468d9c176a930adced55ebfc4def22c
Angel Quintero <angelqv2014@gmail.com>
2023-05-26 19:30:55 -0400

    added collision handler script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,29 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
-    public float angularVelocity;
-    public float speed;
+    [SerializeField] private float spinningVelocity;
+    [SerializeField] private float speed;
     
-    public Transform cam;
+    [SerializeField] private Transform cam;
+    
     private float _horizontalInput;
     private float _verticalInput;
 
     // Start is called before the first frame update
     void Start()
     {
+        cam = GameObject.Find("Main Camera").GetComponent<Transform>();
         _rb = gameObject.GetComponent<Rigidbody>();
+        _rb.maxAngularVelocity = spinningVelocity;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        _rb.angularVelocity = new Vector3(0,angularVelocity,0);
-        _rb.maxAngularVelocity = angularVelocity;
-
+        _rb.angularVelocity = new Vector3(1,spinningVelocity,0);
+        
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
         
@@ -32,7 +35,7 @@ public class PlayerController : MonoBehaviour
         Vector3 camR = cam.right;
         camF.y = 0;
         camR.y = 0;
-        
+
         _rb.AddForce(camF * (_verticalInput * speed) + camR * (_horizontalInput * speed));
     }
 }
